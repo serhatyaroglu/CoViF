c5c4586baabcf892fc633f3cd08cc5a875a8ddf4
serhatyaroglu <61588155+serhatyaroglu@users.noreply.github.com>
2021-01-16 13:36:34 +0300

    dosyalar olusturuldu
    genel ve gunluk

---
+++ CoViF/GenelViewController.swift
@@ -0,0 +1,8 @@
+//
+//  GenelViewController.swift
+//  CoViF
+//
+//  Created by serhat yaroglu on 16.01.2021.
+//
+
+import Foundation
---
+++ CoViF/GunlukViewController.swift
@@ -0,0 +1,8 @@
+//
+//  GunlukViewController.swift
+//  CoViF
+//
+//  Created by serhat yaroglu on 16.01.2021.
+//
+
+import Foundation
